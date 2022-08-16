f3e86e817ab7d02aaca0fb0c8316cae6cba3db29
hassandaru <hassandaru@gmail.com>
2022-08-16 18:49:42 +1000

    tableview populated, image in detail view showing

--- ProgrammingChallenge1/ViewController.swift
+++ ProgrammingChallenge1/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var flagPictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasSuffix("png") {
+                flagPictures.append(item)
+
+            }
+        }
+//
+
+        print(flagPictures)
+    
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flagPictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flagPictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "DetailView") as? DetailViewController {
+            vc.selectedImage = flagPictures[indexPath.row]
+            // 3: now push it onto the navigation controller
+            
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
+    
+    
 }
 
